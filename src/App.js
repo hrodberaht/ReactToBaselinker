c902f5f5ead0da18a3dbf664e5f9cbad8a10e8f5
hrodberaht <robpszczolka@gmail.com>
2018-09-12 18:08:30 +0200

    add live search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,53 @@
-import React, { Component } from 'react';
-import './App.css';
-import SearchProduct from './SearchProduct/SearchProduct'
+import React, { Component } from "react";
+import "./App.css";
+import SearchProduct from "./SearchProduct/SearchProduct";
 class App extends Component {
   state = {
     products: [
       {
         ean: 12312323,
-        name: 'Toy'
+        name: "Toy"
       },
       {
         ean: 12312323,
-        name: 'Doll'
+        name: "Doll"
+      },
+      {
+        ean: 12312323,
+        name: "Toy"
       },
     ]
-  }
-  
-  search = (e) => {
-    console.log(e);
-  }
+  };
 
+  notFilter = {};
 
-  
+  searchHandler = event => {
+    if (event.target.value) {
+      this.notFilter = this.state;
+      
+      const filteredProduct = this.state.products.filter((product) => {
+        return product.name[0] === event.target.value;
+      })
+      console.log(filteredProduct)
+      this.setState({
+        products: filteredProduct
+      });
+      console.log(this.state);
+    } else {
+      this.setState(this.notFilter);
+      console.log('not definied')
+      console.log(this.state);
+
+    }
+  };
+
   render() {
     return (
       <div className="App">
-      <SearchProduct products={this.state.products}/>
+        <SearchProduct
+          changed={this.searchHandler}
+          products={this.state.products}
+        />
       </div>
     );
   }
--- src/SearchProduct/SearchProduct.js
+++ src/SearchProduct/SearchProduct.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react'
 import Product from '../Product/Product'
 
 export default class SearchProduct extends Component {
-  products = this.props.products;
+  
   showProducts = () => {
-    return this.products.map((product, index) => {
+    return this.props.products.map((product, index) => {
       return <Product product={product} key={index} />
     })
   }
+
   
   render() {
     return (
       <div>
-       <input type="text" />
+       <input type="text" onChange={this.props.changed}/>
        {this.showProducts()}
       </div>
     )
